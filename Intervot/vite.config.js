9e017af6de6b7f3cf7c9d7f87ca1438b7ef8547c
ParkYongHo1 <qkaxhf8823@naver.com>
2025-08-03 13:56:17 +0900

    chore: 초기 프로젝트 폴더 구조 세팅 및 PR템플릿 생성

--- Intervot/vite.config.js
+++ Intervot/vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      "@/shared": path.resolve(__dirname, "./src/shared"),
+      "@/app": path.resolve(__dirname, "./src/app"),
+      "@/web": path.resolve(__dirname, "./src/web"),
       "@": path.resolve(__dirname, "./src"),
     },
   },
