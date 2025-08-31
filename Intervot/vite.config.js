c72cf877bf8a6a8a1cd68aeaee48377718c70685
ParkYongHo1 <qkaxhf8823@naver.com>
2025-08-31 15:12:08 +0900

    feat : 빌드 최적화(코드 스프릿팅) 및 운영 보안헤더 설정(Configure CSP, X-Frame-Options, XSS protection)

--- Intervot/vite.config.js
+++ Intervot/vite.config.js
@@ -2,19 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@/shared": path.resolve(__dirname, "./src/shared"),
-      "@/app": path.resolve(__dirname, "./src/app"),
-      "@/web": path.resolve(__dirname, "./src/web"),
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@/shared": path.resolve(__dirname, "./src/shared"),
+            "@/app": path.resolve(__dirname, "./src/app"),
+            "@/web": path.resolve(__dirname, "./src/web"),
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+    server: {
+        port: 5173,
+        open: true,
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ["react", "react-dom"],
+                    utils: ["zustand"],
+                },
+            },
+        },
+    },
 });
